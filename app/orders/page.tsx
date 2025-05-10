1fa836b49bd6d6aab1577a8d07e7d50a20e22eee
jiaamasum <129104663+jiaamasum@users.noreply.github.com>
2025-05-11 00:03:41 +0600

    Update page.tsx

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import prisma from "../lib/db";
 import { redirect } from "next/navigation";
